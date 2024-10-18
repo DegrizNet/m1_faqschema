ad03c6a03de4d8c4de087094879288eaf71a8e3d
DegrizNet <anze@degriz.net>
2024-10-18 13:35:25 +0200

    Initial commit

---
+++ app/code/local/Degriz/CategoryFaq/sql/degriz_dbchange_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,213 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+$installer->addAttribute('catalog_category', 'catalog_question1', array(
+    'type' => 'text',
+    'label' => 'Question 1',
+    'input' => 'text',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => false,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_answer1', array(
+    'type' => 'text',
+    'label' => 'Answer 1',
+    'input' => 'textarea',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => true,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_question2', array(
+    'type' => 'text',
+    'label' => 'Question 2',
+    'input' => 'text',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => false,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_answer2', array(
+    'type' => 'text',
+    'label' => 'Answer 2',
+    'input' => 'textarea',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => true,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_question3', array(
+    'type' => 'text',
+    'label' => 'Question 3',
+    'input' => 'text',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => false,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_answer3', array(
+    'type' => 'text',
+    'label' => 'Answer 3',
+    'input' => 'textarea',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => true,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_question4', array(
+    'type' => 'text',
+    'label' => 'Question 4',
+    'input' => 'text',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => false,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_answer4', array(
+    'type' => 'text',
+    'label' => 'Answer 4',
+    'input' => 'textarea',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => true,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_question5', array(
+    'type' => 'text',
+    'label' => 'Question 5',
+    'input' => 'text',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => false,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_answer5', array(
+    'type' => 'text',
+    'label' => 'Answer 5',
+    'input' => 'textarea',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => true,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_question6', array(
+    'type' => 'text',
+    'label' => 'Question 6',
+    'input' => 'text',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => false,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_answer6', array(
+    'type' => 'text',
+    'label' => 'Answer 6',
+    'input' => 'textarea',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => true,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_question7', array(
+    'type' => 'text',
+    'label' => 'Question 7',
+    'input' => 'text',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => false,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_answer7', array(
+    'type' => 'text',
+    'label' => 'Answer 7',
+    'input' => 'textarea',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => true,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_question8', array(
+    'type' => 'text',
+    'label' => 'Question 8',
+    'input' => 'text',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => false,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->addAttribute('catalog_category', 'catalog_answer8', array(
+    'type' => 'text',
+    'label' => 'Answer 8',
+    'input' => 'textarea',
+    'default' => '',
+    'required' => false,
+    'wysiwyg_enabled' => true,
+    'visible_on_front' => true,
+    'is_html_allowed_on_front' => true,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'FAQ',
+));
+
+$installer->endSetup();
